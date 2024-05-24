c57958937fc82d18b2d6a8d21ac2159ee59dfda0
ravi <ravi.kandukuri66@gmail.com>
2024-05-24 15:22:35 +0530

    update fil

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0cc9838aa7ab1dce7"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "dev-ec2"
+    }
+}
